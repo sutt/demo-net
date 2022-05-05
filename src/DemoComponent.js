1f80e537d7cac3ce1d219bc45f1a4fd2cf8606f6
Will Sutton <wsutton17@gmail.com>
2022-05-04 20:44:49 -0400

    lesson 1

---
+++ src/DemoComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DemoComponent() {
+  return (
+    <div>DemoComponent!</div>
+  )
+}
+
+export default DemoComponent
---
+++ src/MainInfo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MainInfo() {
+  console.log("rendering Main info")
+  return (
+    <main>
+        <p>React is the #1 most popular frontend library in the world</p>
+        <button>Upvote React!</button>
+    </main>
+  )
+}
+
+export default MainInfo
