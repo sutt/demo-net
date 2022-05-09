71cf474df1bc27833fc6da861b5ffb50e1b64546
Will Sutton <wsutton17@gmail.com>
2022-05-09 10:54:33 -0400

    stuff

--- src/App.js
+++ src/App.js
@@ -8,7 +8,43 @@ function App() {
   // hooks for each data type - num/str/arrays/objects
   const [num, setNum] = useState(0)
   const [people, setPeople] = useState(["Alice", "Bob", "Carol", "David", "Evan"])
+  
+  const [subForm, setSubForm] = useState(
+    {
+      winner: {name:"Alice", points:10},
+      loser: {name:"Bob", points:6},
+    }
+  )
 
+  const formHandler = (winStatus, points) => {
+    setSubForm(
+      {...subForm, 
+        [winStatus]: {
+          ...subForm[winStatus],
+           points: points
+          }
+      }
+    )
+  }
+  const [gifs, setGifs] = useState(
+    [{
+      "name": "Beyonce Lemonade Gif",
+      "url": "https://media.giphy.com/media/3o6ozBUuLfzTCngAFi/giphy.gif",
+      "tags": ["Beyonce", "Bey"]
+  }, {
+      "name": "Kim Kardashian Emoji Gif",
+      "url": "https://media.giphy.com/media/jyllN0iwpqydi/giphy.gif",
+      "tags": ["phone", "emoji", "kim kardashian"]
+  }, {
+      "name": "Help Me Cat",
+      "url": "https://media.giphy.com/media/phJ6eMRFYI6CQ/giphy.gif",
+      "tags": ["cat", "kitten", "help", "rescue", "help me"]
+  }, {
+      "name": "Mean Girls Gif",
+      "url": "https://media.giphy.com/media/iDcDa0KQD8Gpq/giphy.gif",
+      "tags": ["mean girls", "wednesdays", "karen", "pink"]
+  }]
+  )
   const changeNumHandler = () => {setNum(num + 1)}
 
 
@@ -29,6 +65,8 @@ function App() {
           ))
         }
       </ul>
+      {gifs.map(item => <img src={item.url} />)}
+
     </div>
     )
 
